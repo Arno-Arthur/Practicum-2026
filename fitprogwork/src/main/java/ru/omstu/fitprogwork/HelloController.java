6d377df5cf602d61fe48454e743c8e31c81c3c95
Aqua <arthur.repin@icloud.com>
2026-02-14 00:56:56 +0600

    LR-1

---
+++ fitprogwork/src/main/java/ru/omstu/fitprogwork/HelloController.java
@@ -0,0 +1,13 @@
+package ru.omstu.fitprogwork;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot on macOS!";
+    }
+}
---
+++ fitprogwork/src/test/java/ru/omstu/fitprogwork/FitprogworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.omstu.fitprogwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitprogworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
